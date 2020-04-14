a5dba5276ed84ddf3c9d3f7adc44934401e7c72d
AndresQA <andresquintero199912@gmail.com>
2020-04-13 21:50:37 -0500

    Rutas y css

--- src/App/App.js
+++ src/App/App.js
@@ -4,15 +4,22 @@ import Performance from '../Performance/Performance';
 import Basic from '../Basic/Basic';
 import Accessories from '../Accessories/Accessories';
 
+import { HashRouter as Router, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       
-      <Home></Home>
-      <Basic></Basic>
-      <Accessories></Accessories>
-      <Performance></Performance>
+      <Router>
+
+      <Route path="/" exact component={Home}/>
+      <Route path="/basic" component={Basic}/>
+      <Route path="/accessories" component={Accessories}/>
+      <Route path="/performance" component={Performance}/>
+
+      </Router>
+
 
 
     </div>
