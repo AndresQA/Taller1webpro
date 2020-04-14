3a4052d661d5c628e001db4124576aeee1b06999
AndresQA <andresquintero199912@gmail.com>
2020-04-13 20:54:17 -0500

    componentes creados

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Home from '../Home/Home';
+import Performance from '../Performance/Performance';
+import Basic from '../Basic/Basic';
+import Accessories from '../Accessories/Accessories';
 
 
 function App() {
   return (
     <div className="App">
+      
+      <Home></Home>
+      <Basic></Basic>
+      <Accessories></Accessories>
+      <Performance></Performance>
+
+
     </div>
   );
 }
