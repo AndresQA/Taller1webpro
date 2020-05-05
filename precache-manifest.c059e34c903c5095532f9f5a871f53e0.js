5a2b977f14cb610e3840ccd515b7d8d8ad348d54
AndresQA <andresquintero199912@gmail.com>
2020-05-04 22:07:01 -0500

    Updates

---
+++ precache-manifest.c059e34c903c5095532f9f5a871f53e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87b83e441b5445120339491ca25d2c0",
+    "url": "/Taller1webpro/index.html"
+  },
+  {
+    "revision": "24824470f8d31f1a62dd",
+    "url": "/Taller1webpro/static/css/main.503c39c7.chunk.css"
+  },
+  {
+    "revision": "9c243a2bbadb87d793d0",
+    "url": "/Taller1webpro/static/js/2.0fb41c26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Taller1webpro/static/js/2.0fb41c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24824470f8d31f1a62dd",
+    "url": "/Taller1webpro/static/js/main.fe35a1d1.chunk.js"
+  },
+  {
+    "revision": "1d7a6a30a8bd226c7a1b",
+    "url": "/Taller1webpro/static/js/runtime-main.765fcd36.js"
+  }
+]);
