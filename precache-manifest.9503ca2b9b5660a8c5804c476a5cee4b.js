80c4b7f2610891ef83361cd8b59ed103ea88844d
AndresQA <andresquintero199912@gmail.com>
2020-05-04 22:59:32 -0500

    Updates

---
+++ precache-manifest.9503ca2b9b5660a8c5804c476a5cee4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019f6353d11004a1c00d9590e7bd58fa",
+    "url": "/Taller1webpro/index.html"
+  },
+  {
+    "revision": "ce06078c6578e2289fb4",
+    "url": "/Taller1webpro/static/css/main.d00be8ec.chunk.css"
+  },
+  {
+    "revision": "9c243a2bbadb87d793d0",
+    "url": "/Taller1webpro/static/js/2.0fb41c26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Taller1webpro/static/js/2.0fb41c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce06078c6578e2289fb4",
+    "url": "/Taller1webpro/static/js/main.b0dd5fa3.chunk.js"
+  },
+  {
+    "revision": "1d7a6a30a8bd226c7a1b",
+    "url": "/Taller1webpro/static/js/runtime-main.765fcd36.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/andresqa.github.io/Taller1webpro/precache-manifest.121f148b106ed5c32e1713754d103e5c.js"
+  "/Taller1webpro/precache-manifest.9503ca2b9b5660a8c5804c476a5cee4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andresqa.github.io/Taller1webpro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Taller1webpro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
